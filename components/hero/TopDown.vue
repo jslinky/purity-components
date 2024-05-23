2ac7c0ae80b214a26292bdf5882771157d1dd675
Justin Small <justinsmall@mac.com>
2024-05-23 13:48:00 +0100

    updates

--- components/hero/TopDown.vue
+++ components/hero/TopDown.vue
@@ -1,17 +1,14 @@
 <template>
   <article class="hero-content wrapper-grid [--end-col:0]">
     <div class="hero-content__container flex-layout-grid justify-center col-span-full">
-      <div class="hero-content__text flex-col flow justify-center flex @container-normal basis-[max-content]">
+      <div class="hero-content__text flex-col flow justify-center flex @container-normal basis-[max-content] w-[min(var(--wrapper-width),calc(100%-(var(--wrapper-padding)*2)))] mx-auto">
         <slot name="hero-content-heading">
           <h1 class="heading-xl">We build your digital future</h1>
           </slot>
           <slot name="hero-content-body">
             <p class="text-xl text-slate max-w-prose text-balance mx-auto">Help us help you take the next step in a journey which will lead you to the final step</p>
           </slot>
-          <slot name="hero-content-button">
-            <div class="flex justify-center">
-              <a href="#" class="btn btn-primary self-start">Find out more</a>
-            </div>        
+          <slot name="hero-content-button">      
           </slot>
       </div>
       <div class="hero-content__image">
